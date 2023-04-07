2b0d36cd69d3b8a09759027a3f659200ae8aa427
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2023-04-07 22:14:59 +0200

    Update dvc pipeline

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 #################################
 
 # Fit a model on the train section
-regr = RandomForestRegressor(n_estimators=50, max_depth=9, random_state=seed)
+regr = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=seed)
 regr.fit(X_train, y_train)
 
 # Report training set score
