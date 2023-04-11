8f699ad8f3855eb561346db56536a959966a2a16
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2023-04-11 19:16:32 +0200

    Change displaying charts

--- train.py
+++ train.py
@@ -64,14 +64,16 @@ axis_fs = 18 #fontsize
 title_fs = 22 #fontsize
 sns.set(style="whitegrid")
 
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(15, 8))
 
-plt.tight_layout()
-plt.savefig("results/feature_importance.png",dpi=120) 
-plt.close()
+sns.barplot(x="importance", y="feature", data=feature_df, ax=ax[0])
+ax[0].set_xlabel('Importance',fontsize = axis_fs) 
+ax[0].set_ylabel('Feature', fontsize = axis_fs)#ylabel
+ax[0].set_title('RF - Feature importance', fontsize = title_fs)
+
+# plt.tight_layout()
+# plt.savefig("results/feature_importance.png",dpi=120) 
+# plt.close()
 
 
 ##########################################
@@ -82,17 +84,17 @@ y_pred = regr.predict(X_test) + np.random.normal(0,0.05,len(y_test))
 y_jitter = y_test + np.random.normal(0,0.05,len(y_test))
 res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
 
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
+sns.scatterplot(x="true", y="pred",data=res_df, ax=ax[1])
+ax[1].set_aspect('equal')
+ax[1].set_xlabel('True wine quality',fontsize = axis_fs) 
+ax[1].set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+ax[1].set_title('RF - Residuals', fontsize = title_fs)
 
 # Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
+ax[1].plot([1, 10], [1, 10], 'black', linewidth=1)
 plt.ylim((2.5,8.5))
 plt.xlim((2.5,8.5))
 
 plt.tight_layout()
-plt.savefig("results/residuals.png",dpi=120) 
+plt.savefig("results/fi_and_residuals.png",dpi=120) 
 
